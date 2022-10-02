6a496963febc841bd77a0ce2c144fc3420b00322
Zeptosec <armandasmatkevicius@gmail.com>
2022-10-02 20:52:23 +0300

    Moved bot code to index

--- index.js
+++ index.js
@@ -6,7 +6,60 @@ const fileSizeLimiter = require('./middleware/fileSizeLimiter');
 const fileNameLimiter = require('./middleware/fileNameLimiter');
 
 //const { AddFileToDB } = require('./database');
-const { saveFile } = require('./bot');
+const { Client, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+  console.log('Ready!');
+  // client.channels.cache.get(process.env.CHANNEL_ID).send({
+  //     files: ['package.json']
+  // })
+  //     .catch(console.error);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply('Server info.');
+  } else if (commandName === 'user') {
+    await interaction.reply('User info.');
+  }
+});
+
+/**
+ * 
+ * @param {File} file the file to save
+ * @returns link to saved file
+ */
+async function saveFile(file) {
+  if (client.isReady) {
+    console.log(file.name);
+    const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+    console.log(channel);
+    let msg = await client.channels.cache.get(process.env.CHANNEL_ID).send({
+      files: [{
+        attachment: file.data,
+        name: file.name
+      }]
+    })
+      .catch(console.error);
+    let uploaded = await msg.attachments.map(a => a.url);
+    return uploaded[0];
+  } else {
+    console.log("not ready");
+  }
+}
+
+// Login to Discord with your client's token
+client.login(process.env.TOKEN);
 
 //const userRoutes = require('./routes/user');
 
